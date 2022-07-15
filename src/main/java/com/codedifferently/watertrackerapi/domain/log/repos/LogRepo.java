17f8e6ed65c139bf3ef4f095e8c4e251679ad003
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2022-07-15 15:28:22 -0400

    updates

--- src/main/java/com/codedifferently/watertrackerapi/domain/log/repos/LogRepo.java
+++ src/main/java/com/codedifferently/watertrackerapi/domain/log/repos/LogRepo.java
@@ -4,9 +4,7 @@ import com.codedifferently.watertrackerapi.domain.log.models.Log;
 import com.codedifferently.watertrackerapi.domain.person.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
 
 public interface LogRepo extends JpaRepository <Log, Long>{
     Iterable<Log> findByPerson(Person person);
-    Iterable<Log> findByDateBetween(Date startDate, Date endDate);
 }
